75305ca4e5de3ffeaca5f5f7b4e70217dc5926c5
Milton Wery <milton_st@hotmail.com>
2023-12-08 19:08:05 -0300

    added env configs with secrets

--- backend-vetmanager/src/config/env-config.validations.ts
+++ backend-vetmanager/src/config/env-config.validations.ts
@@ -4,6 +4,7 @@ const envConfigValidationSchema = z.object({
 	PORT: z.string(),
 	DATABASE_URL: z.string(),
 	HASH_SALT_ROUNDS: z.string(),
+	JWT_SECRET: z.string(),
 });
 
 export const EnvConfigValidation = envConfigValidationSchema.parse;
--- backend-vetmanager/src/config/env.config.ts
+++ backend-vetmanager/src/config/env.config.ts
@@ -2,4 +2,5 @@ export const EnvConfiguration = () => ({
 	port: Number(process.env.PORT),
 	dbURL: process.env.DATABASE_URL,
 	hashSaltRounds: Number(process.env.HASH_SALT_ROUNDS),
+	jwtSecret: process.env.JWT_SECRET,
 });
