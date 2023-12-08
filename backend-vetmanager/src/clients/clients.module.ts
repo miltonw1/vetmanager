106c28ba83dde395b347ccc7418d3106b51d2526
Milton Wery <milton_st@hotmail.com>
2023-12-08 19:07:35 -0300

    Updated imports to relative path

--- backend-vetmanager/src/clients/clients.module.ts
+++ backend-vetmanager/src/clients/clients.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { PrismaModule } from "src/prisma/prisma.module";
+import { PrismaModule } from "../prisma/prisma.module";
 import { ClientController } from "./clients.controller";
 import { ClientService } from "./clients.service";
 
--- backend-vetmanager/src/pets/pets.module.ts
+++ backend-vetmanager/src/pets/pets.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { PrismaModule } from "src/prisma/prisma.module";
+import { PrismaModule } from "../prisma/prisma.module";
 import { PetsController } from "./pets.controller";
 import { PetsService } from "./pets.service";
 
