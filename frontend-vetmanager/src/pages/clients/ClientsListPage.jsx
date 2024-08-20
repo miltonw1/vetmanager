26607db53e6cf80001d6fd0aff035607f1dd083d
Milton Wery <milton_st@hotmail.com>
2024-08-20 20:22:30 -0300

    Added filter function for clients

--- frontend-vetmanager/src/pages/clients/ClientsListPage.jsx
+++ frontend-vetmanager/src/pages/clients/ClientsListPage.jsx
@@ -1,23 +1,50 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useClientStore } from "@s/clients.store";
-
+import { TextInput } from '../../components/common/inputs/TextInput'
 import { MainLayout } from "@/layouts/MainLayout";
 import { ClientCard } from '@/components/clients/ClientCard'
 
 export default function ClientsListPage() {
     const clients = useClientStore((store) => store.clients)
     const getAll = useClientStore((store) => store.getAll)
+    const [filterClient, setFilterClient] = useState('')
 
     useEffect(() => {
         getAll()
     }, [getAll])
+    console.log(filterClient)
 
-    const clientCards = clients.map(client => (<Link to={`/clients/${client.id}`}><ClientCard key={client.id} {...client} /></Link>))
+
+    let clientCards;
+
+    if (filterClient) {
+        const filteredClients = clients.filter(client =>
+            client.name.toLowerCase().includes(filterClient.toLowerCase())
+        );
+        clientCards = filteredClients.map(client => (
+            <Link key={client.id} to={`/clients/${client.id}`}>
+                <ClientCard {...client} />
+            </Link>
+        ));
+    } else {
+        clientCards = clients.map(client => (
+            <Link key={client.id} to={`/clients/${client.id}`}>
+                <ClientCard {...client} />
+            </Link>
+        ));
+    }
+
 
     return (
-        <MainLayout title="Clients">
+        <MainLayout title="Clientes">
+            <TextInput
+                label="Filtrar cliente"
+                className="w-35% my-2"
+                value={filterClient}
+                onChange={event => setFilterClient(event.target.value)}
+            />
             <section className="grid gap-6" style={{ gridTemplateColumns: 'repeat(auto-fill, 360px)' }}>
                 {clientCards}
             </section>
