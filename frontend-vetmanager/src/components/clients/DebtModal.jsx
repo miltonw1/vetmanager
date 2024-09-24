0bc511df4399fa9262330f7a437393c33fbfd7fa
Milton Wery <milton_st@hotmail.com>
2024-09-24 20:06:48 -0300

    Added effects and improved modal

--- frontend-vetmanager/src/components/clients/DebtModal.jsx
+++ frontend-vetmanager/src/components/clients/DebtModal.jsx
@@ -1,32 +1,41 @@
 import { TextInput } from "../common/inputs/TextInput";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export function DebtModal({ client, onCancel, onReduce }) {
-	const [debtReduction, setDebtReduction] = useState(client.debt); // Para manejar el monto a reducir
-	console.log(debtReduction)
+export function DebtModal({ client, onClose }) {
+	const [debtAmount, setDebtAmount] = useState('');
+
+	const isEditing = !!client.debt
+
+	useEffect(() => {
+        if (isEditing) {
+			setDebtAmount(client.debt);
+        }
+    }, [client, isEditing])
+
+
 	return (
 		<div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
 			<div className="bg-cyan-800 h-[30%] w-[50%] p-5 rounded flex flex-col justify-between items-center gap-5 relative">
 				<h1 className="text-3xl text-white">{client.name}</h1>
 				<div>
 					<TextInput
-						value={debtReduction}
-						onChange={(e) => setDebtReduction(Number(e.target.value))} // Actualiza el valor del input
-						placeholder="Monto a reducir"
+						value={debtAmount}
+						onChange={event => setDebtAmount(event.target.value)}
+						placeholder="Monto deuda"
 					/>
 				</div>
 				<div className="flex justify-center gap-4">
 					<button
-						onClick={onCancel}
+						onClick={onClose}
 						className="bg-red-500 text-white px-4 py-2 rounded"
 					>
 						Cancelar
 					</button>
 					<button
-						onClick={() => onReduce(debtReduction)}
+						onClick={onClose}
 						className="bg-green-500 text-white px-4 py-2 rounded"
 					>
-						Reducir deuda
+						{ isEditing ? 'Editar deuda' : 'Agregar deuda' }
 					</button>
 				</div>
 			</div>
