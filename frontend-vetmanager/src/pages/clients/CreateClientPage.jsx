4e946a6cb0f5d09f10f0112b24861b6642f33ac1
Milton Wery <milton_st@hotmail.com>
2024-09-25 19:19:04 -0300

    Removed useless imports

--- frontend-vetmanager/src/pages/clients/CreateClientPage.jsx
+++ frontend-vetmanager/src/pages/clients/CreateClientPage.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import { Link, useNavigate  } from 'react-router-dom'
+import { useNavigate  } from 'react-router-dom'
 
 import { useClientStore } from "@s/clients.store";
-
 import { MainLayout } from "@/layouts/MainLayout";
-import { ClientCard} from '@/components/clients/ClientCard'
 import { ClientForm } from '../../components/clients/ClientForm';
 
 
