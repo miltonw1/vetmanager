9622439c90fc131e9c3c88cfb59546f116075061
Milton Wery <milton_st@hotmail.com>
2024-11-17 21:42:57 -0300

    Added token expiration validation

--- frontend-vetmanager/src/routes/ProtectedRoute.jsx
+++ frontend-vetmanager/src/routes/ProtectedRoute.jsx
@@ -2,7 +2,25 @@ import { Navigate } from 'react-router-dom';
 
 
 const isAuthenticated = () => {
-  return !!localStorage.getItem('token');
+  const token = localStorage.getItem('token');
+  const expiration = localStorage.getItem('token expiration');
+
+  if (!token || !expiration) {
+    return false;
+  }
+
+  const now = new Date();
+  const expirationDate = new Date(parseInt(expiration) * 1000);
+
+
+  if (expirationDate <= now) {
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('token expiration');
+    return false;
+  }
+
+  return true;
 };
 
 export function ProtectedRoute({ children }) {
