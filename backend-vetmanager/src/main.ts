a28b4d4ccc1c08c2f2c282e6f346d1daaf4f6772
Milton Wery <milton_st@hotmail.com>
2025-08-18 20:23:42 -0300

    Added cors allow from everywhere (for testing)

--- backend-vetmanager/src/main.ts
+++ backend-vetmanager/src/main.ts
@@ -6,7 +6,12 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
-	app.enableCors();
+	//Set CORS when deploying
+	app.enableCors({
+		origin: true,
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		credentials: true,
+	});
 	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(process.env.PORT);
 }
